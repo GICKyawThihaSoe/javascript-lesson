eafca518ed99b4f790172766049312b11e8ad939
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-08 14:32:14 +0630

    add-lesson10

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-let name = "Mg Mg"; // variable single value
-let names = ["Mg Mg","Aung Aung","Tun Tun",20,true]; // multiple values
+let person = {
+    name: "Mg Mg",
+    age: 20,
+    married: false,
+    family: ["U Ba","Daw Mya","Su Su"],
+    own:{
+        car: "Toyota",
+        bike: "Moutain Bike"
+    },
+    doIt: function () {
+        console.log(this.name + " is riding a " + this.own.bike);
+    }
+}
 
-console.log(name);
-console.log(names);
+console.log(person.name);
+console.log(person.family[1]);
+console.log(person.own.car);    
+console.log(person.doIt());
 
-let arr = ["Mg Mg","Aung Aung","Tun Tun",20,true];
-console.table(arr);
-console.log(arr[0]);
-console.log(arr[3]);
-console.log(arr[4]);
-
-let arr1 = [
-    [1,2,3],
-    [4,5,6]
-]
-console.table(arr1);
-console.log(arr1[1][2]);
-
-let arr2 = [
-    [1,2,[7,8,9]],
-    [4,5,6]
-]
-
-console.table(arr2);
-console.log(arr2[0][2][1]);
+let json = JSON.stringify(person);
+console.log(json);
+console.log(JSON.parse(json));
