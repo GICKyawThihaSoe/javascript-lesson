8b8397d67879e6cf090513654897dfea2d8d0a4e
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-08 14:50:05 +0630

    add-lesson11

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-let person = {
+const person = {
     name: "Mg Mg",
     age: 20,
-    married: false,
-    family: ["U Ba","Daw Mya","Su Su"],
-    own:{
-        car: "Toyota",
-        bike: "Moutain Bike"
-    },
-    doIt: function () {
-        console.log(this.name + " is riding a " + this.own.bike);
-    }
 }
+person.name = "Kyaw Kyaw"
+console.table(person);
 
-console.log(person.name);
-console.log(person.family[1]);
-console.log(person.own.car);    
-console.log(person.doIt());
+const con = "Mg Mg"; // cannot reassign a constant
+console.log(con); 
 
-let json = JSON.stringify(person);
-console.log(json);
-console.log(JSON.parse(json));
+const array = new Array(1,2,3,4);
+
+array[1] = 20;
+
+console.table(array);
+
+let x = 20;
+let y = 30;
+let a = x + y;
+let b = x - y;
+let c = x * y;
+let d = x / y;
+let e = x % y;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+let ab = 20;
+ab = ab + 1;
+console.log(ab);
+
+let cd = 20;
+cd += 1;
+console.log(cd);
+
+let de = 20;
+de ++;
+console.log(de);
+
+let ef = 20;
+console.log(ef++);
+console.log(ef);
+
+let ac = 20;
+console.log(++ac);
