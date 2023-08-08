d1f718c019635dd30ec4d08b12f3c0dd14902a94
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-08 13:38:13 +0630

    update

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 let x = 20;
-let y = 30;
+let y = 30; // assign = assignment Operator
 let z = x + y;
 console.log(z);
+
+let a = 20; // Number Type
+let name = 'Mg Mg'; // String Type
+
+/*
+    let => variable keyword
+    name => variable identifier name
+    = => Assignment Operator
+    'Mg Mg' => value
+    ; => end of statement
+ */
+
+document.querySelector("#head").innerText = name;
