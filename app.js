33a67fadb8e112ef6ad00f6635f558b0a5835f90
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-08 15:00:54 +0630

    add lesson12

--- app.js
+++ app.js
@@ -1,48 +1,27 @@
-const person = {
-    name: "Mg Mg",
-    age: 20,
-}
-person.name = "Kyaw Kyaw"
-console.table(person);
-
-const con = "Mg Mg"; // cannot reassign a constant
-console.log(con); 
-
-const array = new Array(1,2,3,4);
-
-array[1] = 20;
-
-console.table(array);
+// Conditional Operators
+// ==, ===, >, <, >=, <=, !=
 
 let x = 20;
-let y = 30;
-let a = x + y;
-let b = x - y;
-let c = x * y;
-let d = x / y;
-let e = x % y;
+let y = 20;
+let a = x == y;
+let b = x === y;
+let c = x > y;
+let d = x < y;
+let e = x >= y;
+let f = x <= y;
+let g = x != y;
 
 console.log(a);
 console.log(b);
 console.log(c);
 console.log(d);
 console.log(e);
+console.log(f);
+console.log(g);
 
 let ab = 20;
-ab = ab + 1;
-console.log(ab);
-
-let cd = 20;
-cd += 1;
-console.log(cd);
-
-let de = 20;
-de ++;
-console.log(de);
-
-let ef = 20;
-console.log(ef++);
-console.log(ef);
-
-let ac = 20;
-console.log(++ac);
+let cd = "20";
+let ac = ab === cd; // === (type,value)
+let bd = ab == cd; // == (value)
+console.log(ac);
+console.log(bd);
