457974f91104c78882dbe50357d5c8f19e83182c
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-08 13:50:04 +0630

    add-lesson-7

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-let x = 20;
-let y = 30; // assign = assignment Operator
-let z = x + y;
-console.log(z);
+let num = 20; // Number Type
+let name = "Mg Mg"; // String Type
+let bol = 4 > 5; // Boolean Type | Boolean expression 
 
-let a = 20; // Number Type
-let name = 'Mg Mg'; // String Type
+console.log(typeof num);
+console.log(typeof name);
+console.log(typeof bol);
 
-/*
-    let => variable keyword
-    name => variable identifier name
-    = => Assignment Operator
-    'Mg Mg' => value
-    ; => end of statement
- */
+let a; // declear
+a = "Kyaw Kyaw"; // assign
 
-document.querySelector("#head").innerText = name;
+console.log(a);
+
+let car = "Toyota"; // initialize
+car = "Suzuki"; // reassign
+
+console.log(car);
+
+let b; // declare
+let age = 21; // initialize
+
+b = age > 22 ? "Bo Bo" : "Ko Ko"; // ternary operator
+
+console.log(b);
