da4d2ccf471204d01d928cc39545f06cf57bcd5e
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-08 15:41:45 +0630

    add lesson16

--- app.js
+++ app.js
@@ -4,21 +4,6 @@
 // for , while , dowhile
 // for in , foreach
 
-let color = "red";
-
-switch(color){
-    case "green":
-        console.log("It is green");
-        break;
-    case "blue":
-        console.log("It is blue");
-        break;
-    case "yellow":
-        console.log("It is yellow");
-        break;
-    case "red":
-        console.log("It is red");
-        break;
-    default:
-        console.log("I can't guess!");
+for (let i = 0; i < 5; i++){
+    console.log("Hello world" + i)
 }
