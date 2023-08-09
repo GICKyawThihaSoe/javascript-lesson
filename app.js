450bd9c0b33caf7b45cbc4f2155369bfc7751d27
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-09 14:00:42 +0630

    add lesson32

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-let now = new Date();
-console.log(now);
-console.log(now.getDay());
-console.log(now.getMonth() + 1);
-console.log(now.getDate());
-console.log(now.getFullYear());
-console.log(now.getHours());
-console.log(now.getMinutes());
-console.log(now.getSeconds());
-console.log(now.getMilliseconds());
-console.log(now.getTime());
-/*****/
-let dd = new Date();
-let ddc = dd.toString();
-console.log(typeof ddc);
-/*****/
-let da = new Date("02/24/2003");
-console.log(da);
-/*****/
-let dab = new Date("February 24 2003 10:33:22");
-console.log(dab);
-/*****/
-let aa = new Date("February 24 2003 10:33:22");
-aa.setDate(11);
-console.log(aa);
+let numbers = [12,43,55,1,6,9,2,5];
+numbers.sort();
+console.log(numbers);
+/*******/
+let numbers2 = [12,43,55,1,6,9,2,5];
+function filter(x,y){
+    return x - y;
+}
+numbers2.sort(filter);
+console.log(numbers2);
+/*******/
+let numbers3 = [12,43,55,1,6,9,2,5];
+function filter2(x,y){
+    return y - x;
+}
+numbers3.sort(filter2);
+console.log(numbers3);
+/*******/
+let num = [12,43,55,1,6,9,2,5];
+num.sort((x,y)=> x - y);
+console.log(num);
+/*******/
+let num2 = [12,43,55,6,9,2,5];
+let result = num2.find(x => x < 12);
+console.log(result);
+/*******/
+let ac = [12,43,55,6,9,2,5];
+let ary = new Array(123,444,222);
+let resultto = ac.concat(ary);
+console.log(resultto);
