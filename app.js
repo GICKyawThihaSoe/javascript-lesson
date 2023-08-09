6bd87637d145b141f7da256d6cec05d480b1c0c7
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-09 08:30:45 +0630

    add lesson24

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-// Premitive Type
-let name = "Mg Mg"; // String
-let age = 20; // Number
-let con = false; // Boolean
-let data = null; // Object
-let dd; // undefined
-let sym = Symbol(); // symbol
+let num = 2.4;
 
-// Refrence Type
-let arry = ["one","two","three","four","five"];
-let person = {
-    name: "Mg Mg",
-    age: 20,
+console.log(Math.round(num));
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
+console.log(Math.sqrt(num));
+console.log(Math.abs(num));
+console.log(Math.pow(8,2));
+console.log(Math.min(3,5));
+console.log(Math.max(3,5));
+
+for(let i = 0; i < 10; i++){
+    let num = Math.ceil((Math.random() * 6) + 0);
+    console.log(num);
 }
-let audio = new Audio();
-let date = new Date();
-
-console.log(date);
