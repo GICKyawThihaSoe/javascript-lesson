06b0cbd2cf3a0c190c5923a75b51e43f0dec8f95
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-09 14:50:46 +0630

    add lesson36

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-let name = prompt("What is your name?");
-document.querySelector("#head").innerHTML = name;
-
-let con = confirm("is your age over 18");
-let answer = con ? "Yes over 18" : "No under 18";
-document.querySelector(".para").innerHTML = answer;
+console.log(document.URL);
+console.log(document.body);
