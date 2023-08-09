8b57b508480483bd913f4fc056d372d78b37d5fb
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-09 08:52:23 +0630

    add lesson26

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
 let num = 20;
-
-console.log(num);
-console.log(typeof num);
-
-let str = num.toString();
-
-console.log(str);
-console.log(typeof str);
-console.log("********");
-let string = "30";
-let number = Number(string);
-console.log(number);
-console.log(typeof number);
-console.log("********");
-let nan = "20hello";
-let NaNmuber = Number(nan);
-console.log(NaNmuber);
-console.log(typeof NaNmuber)
-console.log("********");
-let a = "20";
-let b = parseInt(a);
-console.log(b);
-console.log(typeof b);
-console.log("********");
-let c = "20.212213";
-let d = parseFloat(c);
-console.log(d);
-console.log(typeof d);
-console.log("********");
-let ab = "20.212213";
-let cd = Number(ab).toFixed(2);
-console.log(cd);
-console.log(typeof cd);
+let name = num == 20 ? "Mg Mg" : "Aung Aung";
+console.log(name);
