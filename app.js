0e24782d86ba2128df59189fd46b7b4df31470c0
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-09 09:26:26 +0630

    add lesson27

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-let num = 20;
-let name = num == 20 ? "Mg Mg" : "Aung Aung";
-console.log(name);
+function doIt(){
+  let x = 10;
+  let y = 20;
+  let z = x + y;
+
+  console.log(z);
+}
+
+doIt();
+
+function dod(){
+    return 20;
+}
+
+let total = dod() + 20;
+console.log(total);
+console.log("******");
+function test(name){
+    return "Mr." + name;
+}
+console.log(test("Mg Mg"));
+console.log("******");
+function oktt(name,age){
+    return "Name is " + name + " And " + "Age is " + age;
+}
+console.log(oktt("Aung Aung",20));
