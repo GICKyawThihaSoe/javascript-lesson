45db75f8d74067723a74979f82a2081d910bc486
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-09 15:01:52 +0630

    add lesson37

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-console.log(document.URL);
-console.log(document.body);
+let scripts = document.scripts;
+let scriptsArray = Array.from(scripts);
+scriptsArray.forEach((scripts)=>{
+    console.log(scripts.src);
+})
+
+let btn = document.querySelector(".btn");
+btn.onclick = () =>{
+    alert(123);
+}
+let btn2 = document.querySelector("#btn");
+btn2.onclick = ()=>{
+    alert(456);
+}
