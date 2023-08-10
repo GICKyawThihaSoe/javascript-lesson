4bb6ddc8b3f75ffea5ab510613b90e8f97e7f1e5
GICKyawThihaSoe <kyawthihasoe@gicjp.com>
2023-08-10 08:30:50 +0630

    add tailwind-css

--- app.js
+++
@@ -1,8 +0,0 @@
-let oldH1 = document.querySelector("#head");
-let text = document.createTextNode("New Header");
-let newH1 = document.createElement("h1");
-newH1.appendChild(text);
-newH1.className = "biber";
-newH1.id = "bib";
-let body = oldH1.parentNode;
-body.replaceChild(newH1,oldH1);
